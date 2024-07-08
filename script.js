cfbd1763be56975282d97ca097dfd9236d5fda3b
MATEUS JAIRAN DE SOUSA RODRIGUES <MATEUS0689@DIGITALCOLLEGE.LOCAL>
2024-07-08 16:51:27 -0300

    introdução do codigo javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const opcoes = {
+    1: {nome: "pedra", img: "img/pedra.png", pedra: "EMPATE", papel: "PERDEU", tesoura:"GANHOU"},
+    2: {nome: "papel", img:"img/papel.png", pedra: "GANHOU", papel:"EMPATE", tesoura:"PERDEU"},
+    3: {nome: "tesoura", img:"img/tesoura.png", pedra:"PERDEU", papel:"GANHOU",tesoura:"EMPATE"},
+}
+
+let escolherJogador = {}
+let escolhaComputador = {}
+
+function escolherJogada(num) {
+    escolherJogador = opcoes[num]
+}
+
+let numComp = Math.floor((Math.random()*3)+1)
+
+escolhaComputador = opcoes[numComp]
